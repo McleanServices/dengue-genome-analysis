4c44ea2518eb69a450ac1849c62b6ee4f1850682
TyreceMclean <73130117+McleanServices@users.noreply.github.com>
2024-11-11 21:27:35 -0400

    Update settings.py

--- bioinformatique2/settings.py
+++ bioinformatique2/settings.py
@@ -144,3 +144,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://keigonwilson.com',
+]
+
+
